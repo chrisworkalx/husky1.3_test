7ef96800892dbd19bf4cd3b30bbd07d6b078a5b8
chrisworkalx <857717575@qq.com>
2022-04-03 15:03:10 +0800

    add: eslint rule

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: ['vue']
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-console.log('good')
+console.log('good');
 console.log('good123jjjj');
-console.log(7777)
+console.log(7777);
